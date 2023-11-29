4e292c97046fb19117d4cf3dfd1610436cb2818a
dev_vitorh <vitoor.hmateus@gmail.com>
2023-11-29 08:54:53 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Base : ");
+            double baseDoTriangulo = double.Parse(Console.ReadLine());
+            Console.Write("Altura: ");
+            double alturaDoTriangulo = double.Parse(Console.ReadLine());
+
+            double area = (baseDoTriangulo * alturaDoTriangulo) / 2;
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine($"Base : {baseDoTriangulo}");
+            Console.WriteLine($"Altura : {alturaDoTriangulo}");
+            Console.WriteLine($"Area : {area} cm²");
+
+        }
+    }
+}
